6e0a563b8a82d727959ff1fe2dfc3c080b46c010
tojo chacko <tojo.c@directi.com>
2013-09-16 19:48:48 +0530

    Abstracted the request module part into a seperate function since if an error was found, I could not get the exact url for which error was generated.

--- index.js
+++ index.js
@@ -6,22 +6,28 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', function(chunk) {
     var urlArr = chunk.split('\n');
 
-    //@TODO: remove the last element of the array
+    //remove the last element since its an empty string
+    urlArr.pop();
     chkUrl(urlArr); 
 });
 
 process.stdin.on('end', function() {
-    process.stdout.write('Process finished');
+    console.log('Process finished');
 });
 
 var chkUrl = function(arr) {
     var urlToChk;
     for(var i=0; i<arr.length;i++) {
         urlToChk = arr[i];
-        request(urlToChk, function (error, response, body) {
-            if(error || response.statusCode != 200) {
-                process.stdout.write('Rouge URL: '+urlToChk);
-            }
-        });
+        requestURL(urlToChk);
     }
+}
+
+var requestURL = function(url) {
+    request(url, function (error, response, body) {
+        if(error || response.statusCode != 200) {
+            console.log('Rougue URL: '+url);
+            console.log(JSON.stringify(error));
+        }
+    });
 }
