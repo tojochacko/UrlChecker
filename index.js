46fc40439e4f076d02bfbe8794d4e9f2751e507a
tojo chacko <tojo.c@directi.com>
2013-09-22 00:35:00 +0530

    Used an htmlparser library to parse for href in html files.

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
 var request = require('request');
+var htmlparser = require("htmlparser2");
+var data = '';
+
+var parser = new htmlparser.Parser({
+    onopentag: function(name, attribs){
+        if(name === "a"){
+            if(beginswithHttp(attribs.href)) {
+                //console.log(attribs.href);
+                requesturl(attribs.href); 
+            }
+        }
+    },
+});
+
+function requesturl(url) {
+    request(url, function (error, response, body) {
+        if(error || response.statusCode != 200) {
+            console.log('Rougue Url: '+url);
+            console.log(JSON.stringify(error));
+        }
+    });
+}
+
+function beginswithHttp(url) {
+    var regex = new RegExp("/((http|https):\/\/(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/");
+
+    return regex.test(url);
+} 
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', function(chunk) {
-    //@TODO: functionality to check for urls in a file still left
-
-    var urlArr = chunk.split('\n');
-
-    //remove the last element since its an empty string
-    urlArr.pop();
-    chkUrl(urlArr); 
+    data += chunk;
 });
 
 process.stdin.on('end', function() {
-    console.log('Process finished');
+    parser.write(data);
+    parser.end();
 });
-
-var chkUrl = function(arr) {
-    var urlToChk;
-    for(var i=0; i<arr.length;i++) {
-        urlToChk = arr[i];
-        requestURL(urlToChk);
-    }
-}
-
-var requestURL = function(url) {
-    request(url, function (error, response, body) {
-        if(error || response.statusCode != 200) {
-            console.log('Rougue URL: '+url);
-            console.log(JSON.stringify(error));
-        }
-    });
-}
